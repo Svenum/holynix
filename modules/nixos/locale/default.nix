8ff6abc7e51f9836ffd463c9ea0c19a7feb42564
Svenum <s.ziegler@holypenguin.net>
2024-04-27 00:34:42 +0200

    fix config

--- modules/nixos/locale/default.nix
+++ modules/nixos/locale/default.nix
@@ -9,30 +9,42 @@ let
   singleLocale = [ "de_DE" "en_US" ];
 in
 {
-  options.holynix.locale = mkOption {
-    type = enum [ "de_DE" "en_DE" "en_US" ];
-    default = "de_DE";
-  };
+  options.holynix.locale = {
+    name = mkOption {
+      type = enum [ "de_DE" "en_DE" "en_US" ];
+      default = "de_DE";
+    };
+    tz = mkOption {
+      type = str;
+      default = "Europe/Berlin";
+    };
+  }; 
 
   config = {
+    # Configure Timezone
+    time.timeZone = cfg.tz;
+    
+    # Configure Keyboard layout
+    console.useXkbConfig = true;
+
     i18n = {
       supportedLocales = [
         "C.UTF-8/UTF-8"
       ]
-      ++ lists.optional (builtins.elem cfg singleLocale) "${cfg}.UTF-8/UTF-8"
-      ++ lists.optionals (cfg == "en_DE") [ "en_US.UTF-8/UTF-8" "de_DE.UTF-8/UTF-8" ];
+      ++ lists.optional (builtins.elem cfg.name singleLocale) "${cfg.name}.UTF-8/UTF-8"
+      ++ lists.optionals (cfg.name == "en_DE") [ "en_US.UTF-8/UTF-8" "de_DE.UTF-8/UTF-8" ];
 
-      defaultLocale = if (builtins.elem cfg singleLocale) then
-        "${cfg}.UTF-8/UTF-8"
-      else if (cfg == "en_DE") then
+      defaultLocale = if (builtins.elem cfg.name singleLocale) then
+        "${cfg.name}.UTF-8/UTF-8"
+      else if (cfg.name == "en_DE") then
         "en_US.UTF-8"
       else
         ""
       ;
 
-      extraLocaleSettings = if (builtins.elem cfg singleLocale) then 
-        { LC_ALL = "${cfg}.UTF-8"; }
-      else if (cfg == "en_DE") then
+      extraLocaleSettings = if (builtins.elem cfg.name singleLocale) then 
+        { LC_ALL = "${cfg.name}.UTF-8"; }
+      else if (cfg.name == "en_DE") then
         {
           LC_ADDRESS = "de_DE.UTF-8";
           LC_IDENTIFICATION = "de_DE.UTF-8";
@@ -49,7 +61,7 @@ in
     };
     
     services.xserver.xkb = mkIf desktopCfg.enable {
-      layout = strings.toLower (lists.last (strings.splitString "_" cfg));
+      layout = strings.toLower (lists.last (strings.splitString "_" cfg.name));
       variant = "";
     };
   };
--- systems/x86_64-linux/Yon/default.nix
+++ systems/x86_64-linux/Yon/default.nix
@@ -7,10 +7,10 @@
   ];
 
   holynix = {
-  #  boot.secureBoot = true;
-  #  desktop.plasma.enable = true;
-  #  shell.zsh.enable = true;
-  #  locale = "en_DE";
+    boot.secureBoot = true;
+    desktop.plasma.enable = true;
+    shell.zsh.enable = true;
+    locale.name = "en_DE";
     users."sven" = {
       isGuiUser = true;
       isSudoUser = true;
@@ -19,56 +19,56 @@
         userEmail = "s.ziegler@holypenguin.net";
       };
     };
-  #  wireguard.enable = true;
-  #  tools = {
-  #    nvim.enable = true;
-  #    tmux.enable = true;
-  #    flatpak.enable = true;
-  #    cliTools.enable = true;
-  #  };
+    wireguard.enable = true;
+    tools = {
+      nvim.enable = true;
+      tmux.enable = true;
+      flatpak.enable = true;
+      cliTools.enable = true;
+    };
 
-  #  bluetooth.enable = true;
+    bluetooth.enable = true;
 
-  #  kvm = {
-  #    enable = true;
-  #    vfioPCIDevices = [
-  #      "1002:ab30"
-  #      "1002:7480"
-  #    ];
-  #  };
+    kvm = {
+      enable = true;
+      vfioPCIDevices = [
+        "1002:ab30"
+        "1002:7480"
+      ];
+    };
 
-  #  gpu.amd.enable = true;
+    gpu.amd.enable = true;
 
-  #  powerManagement.enable = true;
+    powerManagement.enable = true;
 
-  #  scanner.enable = true;
-  #  printer = {
-  #    enable = true;
-  #    defaultPrinter = "Epson_ET-2720-Series";
-  #    printers = [ 
-  #      {
-  #        name = "Epson_ET-2720-Series";
-  #        deviceUri = "https://pr-epson.intra.holypenguin.net:631/ipp/print";
-  #        description = "Epson ET-2720"; 
-  #        model = "epson-inkjet-printer-escpr/Epson-ET-2720_Series-epson-escpr-en.ppd";
-  #      }
-  #      {
-  #        name = "HP_Officejet_5740-Series";
-  #        deviceUri = "https://pr-hp.intra.holypenguin.net/ipp/printers";
-  #        description = "HP Officejet 5740"; 
-  #        model = "HP/hp-officejet_5740_series.ppd.gz";
-  #      }
-  #    ];
-  #  };
+    scanner.enable = true;
+    printer = {
+      enable = true;
+      defaultPrinter = "Epson_ET-2720-Series";
+      printers = [ 
+        {
+          name = "Epson_ET-2720-Series";
+          deviceUri = "https://pr-epson.intra.holypenguin.net:631/ipp/print";
+          description = "Epson ET-2720"; 
+          model = "epson-inkjet-printer-escpr/Epson-ET-2720_Series-epson-escpr-en.ppd";
+        }
+        {
+          name = "HP_Officejet_5740-Series";
+          deviceUri = "https://pr-hp.intra.holypenguin.net/ipp/printers";
+          description = "HP Officejet 5740"; 
+          model = "HP/hp-officejet_5740_series.ppd.gz";
+        }
+      ];
+    };
   };
 
   # enable Steam input
-  #hardware.steam-hardware.enable = true;
-  #programs.gamescope.enable = true;
+  hardware.steam-hardware.enable = true;
+  programs.gamescope.enable = true;
 
   # enable solaar
-  #programs.solaar.enable = true;
+  programs.solaar.enable = true;
   
   # Enable Waydroid
-  #virtualisation.waydroid.enable = true;
+  virtualisation.waydroid.enable = true;
 }
