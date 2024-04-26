a3801d654613e143a84af1070dfccfd652879187
Svenum <s.ziegler@holypenguin.net>
2024-04-27 00:47:37 +0200

    add sven to kvmusers

--- systems/x86_64-linux/Yon/default.nix
+++ systems/x86_64-linux/Yon/default.nix
@@ -14,6 +14,7 @@
     users."sven" = {
       isGuiUser = true;
       isSudoUser = true;
+      isKvmUser = true;
       git = {
         userName = "Svenum";
         userEmail = "s.ziegler@holypenguin.net";
