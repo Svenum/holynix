6b33a1fe1ab336f33d348858f2d95afe420bd823
Svenum <s.ziegler@holypenguin.net>
2024-05-31 23:03:33 +0200

    fix tmux bar

--- modules/nixos/tools/tmux/default.nix
+++ modules/nixos/tools/tmux/default.nix
@@ -29,8 +29,8 @@ in
         set -g repeat-time 700
         set -g mouse on
         set -g @catppuccin_flavour '${themeCfg.flavour}'
+        set -g @catppuccin_status_modules_right "application session user host date_time"
         set -g @catppuccin_date_time_text "%d.%m.%Y %H:%M"
-        set -g @catppuccin_status_modules "application user date_time"
         
         set -g @catppuccin_window_right_separator " "
         set -g @catppuccin_window_left_separator "█"
@@ -38,8 +38,7 @@ in
         set -g @catppuccin_window_left_separator_inverse "yes"
         
         set -g @catppuccin_status_left_separator " "
-        set -g @catppuccin_status_right_separator " "
-        set -g @catppuccin_status_right_separator_inverse "yes"
+        set -g @catppuccin_status_right_separator "█ "
 
         set -g @catppuccin_status_fill "all"
         set -g @catppuccin_status_connect_separator "no"
