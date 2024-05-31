2465cd6aa8bd5ae0e3e2420fcd9bf48b30890d7b
Svenum <s.ziegler@holypenguin.net>
2024-05-31 23:37:43 +0200

    split tmux in current dir

--- modules/nixos/tools/tmux/default.nix
+++ modules/nixos/tools/tmux/default.nix
@@ -27,15 +27,25 @@ in
         vim-tmux-navigator
       ];
       extraConfigBeforePlugins = ''
+        # TMUX
         set -g repeat-time 700
         set -g mouse on
+
+        bind c new-window -c "#{pane_current_path}"
+        bind '"' split-window -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+
+        bind-key ! break-pane -d -n _hidden_pane
+        bind-key @ join-pane -s $.0
+
+        # Catppuccin
         set -g @catppuccin_flavour '${themeCfg.flavour}'
         set -g @catppuccin_status_modules_right "application session user host date_time"
         set -g @catppuccin_date_time_text "%d.%m.%Y %H:%M"
         
         set -g @catppuccin_window_right_separator " "
         set -g @catppuccin_window_left_separator "█"
-        set -g @catppuccin_window_middle_separator " █"
+        set -g @catppuccin_window_middle_separator "█"
         
         set -g @catppuccin_status_left_separator " "
         set -g @catppuccin_status_right_separator "█"
