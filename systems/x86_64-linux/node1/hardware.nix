4c8c5def4e9d8427317be1872bf18cd589f709be
Svenum <s.ziegler@holypenguin.net>
2024-06-10 21:42:29 +0200

    fix typo

--- systems/x86_64-linux/node1/hardware.nix
+++ systems/x86_64-linux/node1/hardware.nix
@@ -5,7 +5,7 @@
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "ahci", "xhci_pci", "virtio_pci", "sr_mod", "virtio_blk" ];
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
   boot.initrd.kernelModules = [];
   boot.kernelModules = [];
   boot.extraModulePackages = [];
