44374644374eecd4609ae19bc4261913c5e089d7
Svenum <s.ziegler@holypenguin.net>
2024-06-10 21:41:32 +0200

    add kernel modules

--- systems/x86_64-linux/node1/hardware.nix
+++ systems/x86_64-linux/node1/hardware.nix
@@ -1,6 +1,15 @@
-{ ... }:
+{ modulesPath, ... }:
 
 {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [ "ahci", "xhci_pci", "virtio_pci", "sr_mod", "virtio_blk" ];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = [];
+  boot.extraModulePackages = [];
+
   # Filesystems
   fileSystems."/" = {
     label = "NixOS";
