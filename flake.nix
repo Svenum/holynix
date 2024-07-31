44f0dfeeccd6ce5eecfe6bfa75457446553161cc
Svenum <s.ziegler@holypenguin.net>
2024-07-31 21:26:36 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -108,16 +108,11 @@
   };
 
   nixConfig = {
-    extra-trusted-substituters = [
+    extra-substituters = [
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-    extra-allowed-users = [
-      "sven"
-      "sudouser"
-      "martinn"
     ];
   };
 
