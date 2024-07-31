8beae61f5b32252c963072fe620231cbd273982c
Svenum <s.ziegler@holypenguin.net>
2024-07-31 21:39:04 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -106,17 +106,4 @@
       fw-fanctrl.nixosModules.default
     ];
   };
-
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    trusted-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
 }
