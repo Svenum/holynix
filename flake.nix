6c11106894c8070f9a86cdba523e1d3d0749138f
Svenum <s.ziegler@holypenguin.net>
2024-07-31 21:13:59 +0200

    add allowed users

--- flake.nix
+++ flake.nix
@@ -114,6 +114,11 @@
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    extra-allowed-users = [
+      "sven"
+      "sudouser"
+      "martinn"
+    ];
   };
 
 }
