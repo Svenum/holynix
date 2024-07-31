2caac65914a4829954ad0566599892f5b4052a2d
Svenum <s.ziegler@holypenguin.net>
2024-07-31 21:33:55 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -111,11 +111,11 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
     ];
+    trusted-substituters = [
+      "https://nix-community.cachix.org"
+    ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-    extra-trusted-users = [
-      "@wheel"
     ];
   };
 
