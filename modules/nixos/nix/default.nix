805d17d4c3d9a6512ce63c4836b723e740243519
Svenum <s.ziegler@holypenguin.net>
2024-08-03 01:05:49 +0200

    add own cache

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -33,10 +33,16 @@ in
       substituters = [
         # nix community's cache server
         "https://nix-community.cachix.org"
+
+        # own cache
+        "https://s3.holypenguin.net/nix-cache/"
       ];
       trusted-public-keys = [
         # nix community's cache server public key
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+
+        # own cache
+        "s3.holypenguin.net-1:FazWVlpLLuiIw12YuKaWJ0pUI8NGe50txqlca62lFrA="
       ];
     };
   };
