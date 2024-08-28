3f4174863690f8428e34a07fbcfbb20254856de1
Svenum <s.ziegler@holypenguin.net>
2024-08-28 14:41:59 +0200

    add ansible-nix-shell

--- modules/nixos/systemType/server/default.nix
+++ modules/nixos/systemType/server/default.nix
@@ -4,6 +4,40 @@ with lib;
 with lib.types;
 let
   cfg = config.holynix.systemType.server;
+
+  ansible-nix-shell = pkgs.writeShellScrtipBin "ansible-nix-shell" ''
+    # Initialisiere Variablen    
+    p_value=""    
+    r_value=""    
+    collecting_p=false    
+    collecting_r=false    
+         
+    # Durchlaufe alle Argumente    
+    while [[ $# -gt 0 ]]; do    
+      case "$1" in    
+        -p)    
+          collecting_p=true    
+          collecting_r=false    
+          shift    
+          ;;    
+        -r)    
+          collecting_r=true    
+          collecting_p=false    
+          shift    
+          ;;    
+        *)    
+          if $collecting_p; then    
+            p_value="$p_value $1"    
+          elif $collecting_r; then    
+            r_value="$r_value $1"    
+          fi    
+          shift    
+          ;;    
+      esac    
+    done    
+         
+    nix-shell -p $p_value --run "$r_value"
+  '';
 in
 {
   options.holynix.systemType.server = {
@@ -30,6 +64,7 @@ in
 
     environment.systemPackages = with pkgs; mkIf cfg.ansibleTarget [
       python3
+      ansible-nix-shell
     ];
   };
 }
