5111bb083cf755bddd1a842592a6ea380fc07e29
Svenum <s.ziegler@holypenguin.net>
2024-09-07 12:22:42 +0200

    change ip

--- systems/aarch64-linux/srv-raspi5/default.nix
+++ systems/aarch64-linux/srv-raspi5/default.nix
@@ -34,13 +34,11 @@
   networking = {
     interfaces = {
       enp1s0.ipv4.addresses = [{
-        #address = "172.16.0.12";
-        address = "172.17.0.89";
+        address = "172.16.0.13";
         prefixLength = 24;
       }];
     };
-    #defaultGateway = "172.16.0.1";
-    defaultGateway = "172.17.0.1";
+    defaultGateway = "172.16.0.1";
     nameservers = [ "172.16.0.3" "172.16.0.4" ];
     useDHCP = false;
   };
