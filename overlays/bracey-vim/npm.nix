5d066a4691d37c72c7a11827b5f7c06461422d24
Svenum <s.ziegler@holypenguin.net>
2024-09-23 11:33:23 +0200

    downgrade version

--- overlays/bracey-vim/npm.nix
+++ overlays/bracey-vim/npm.nix
@@ -2,7 +2,7 @@
 
 {pkgs ? import <nixpkgs> {
     inherit system;
-  }, system ? builtins.currentSystem, nodejs ? pkgs."nodejs_14"}:
+  }, system ? builtins.currentSystem, nodejs ? pkgs."nodejs_16"}:
 
 let
   nodeEnv = import ./node-env.nix {
