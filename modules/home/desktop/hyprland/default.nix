18015c383cfee402ddf3e16dd5a5b6dac5864920
Svenum <s.ziegler@holypenguin.net>
2024-09-30 17:47:11 +0200

    add first shortcuts

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -31,22 +31,29 @@ in
           kb_layout = strings.toLower (lists.last (strings.splitString "_" localeCfg.name));
 
           follow_mouse = 1;
+          touchpad = {    
+            natural_scroll = "no";    
+          };
+          sensitivity = 0; 
         };
 
         gestures = {
           workspace_swipe = true;
         };
 
-        monitor = [
-          ",preferred,auto,auto"
-        ] ++ cfg.monitors;
-
         general = {
           gaps_in = 5;
           gaps_out = 20;
           border_size = 2;
+          col.active_border = "rgba(#2d1d7a) rgba(#df8046) 45deg";  
+          col.inactive_border = "rgba(595959aa)";
           layout = "dwindle";
+          resize_on_border = true;
         };
+
+        monitor = [
+          ",preferred,auto,auto"
+        ] ++ cfg.monitors;
 
         decoration = {
           rounding = 10;
@@ -56,11 +63,47 @@ in
           drop_shadows = true;
           shadow_range = 4;
           shadow_render_power = 3;
+      
+          blur = {    
+            enabled = true;
+            size = 3;
+            passes = 1;
+          };
+      
+          col.shadow = "rgba(1a1a1aee)";
         };
 
+        animations = {
+          enabled = true;
+          bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+      
+          animation = [
+            "windows, 1, 7, myBezier"
+            "windowsOut, 1, 7, default, popin 80%"
+            "border, 1, 10, default"
+            "borderangle, 1, 8, default"
+            "fade, 1, 7, default"
+            "workspaces, 1, 6, default"
+          ];
+        };
+      
+        dwindle = {
+          pseudotile = true;
+          preserve_split = true;
+        };
+        
         env = [
           "XCURSOR_SIZE,24"
           "HYPRCURSOR_SIZE,24"
+        ];
+
+        "$mainMod" = "ALT1";
+        "$secondMod" = "Win";
+
+        bind = [
+          "CONTROL, $mainMod, T, exec, kitty"
+          "CONTROL, $mainMod, B, exec, xdg-open https://"
+          "$mainMod, Q, killactive"
         ];
       };
     };
