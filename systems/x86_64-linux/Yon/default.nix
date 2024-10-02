7cbcc545501c4564b630f1bed1d887ffc7969829
Svenum <s.ziegler@holypenguin.net>
2024-10-02 10:03:06 +0200

    add boerg

--- systems/x86_64-linux/Yon/default.nix
+++ systems/x86_64-linux/Yon/default.nix
@@ -16,13 +16,23 @@
     shell.zsh.enable = true;
     locale.name = "en_DE";
     systemType.laptop.enable = true;
-    users."sven" = {
-      isGuiUser = true;
-      isSudoUser = true;
-      isKvmUser = true;
-      git = {
-        userName = "Svenum";
-        userEmail = "s.ziegler@holypenguin.net";
+    users = {
+      "sven" = {
+        isGuiUser = true;
+        isSudoUser = true;
+        isKvmUser = true;
+        git = {
+          userName = "Svenum";
+          userEmail = "s.ziegler@holypenguin.net";
+        };
+      };
+      "boerg" = {
+        isGuiUser = false;
+        isSudoUser = false;
+        isKvmUser = false;
+        authorizedKeys = [
+          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDhIrnXyYZ63yo/Y2XqiPiQ5uOviP6pVYLxx+Iyuo5DjiGsjR/FOG6wWdeTtlpMbEinqFBtq5d3wGqDtQBak9IDsqJ/u9khT7fsQiykrxIxemSv8bCzvXeh9rnFuAA6cjvPwL9Ie7g38W7GHP5aJjLMx6vUiRHafD+5T37uYK2VUhVG8XTbygS4C+k3DOQ36R+whHoLeu0okFhTt6nu2IX2qx/j8kllOwCVq7AjbPAQJmDPvEOVZONHRDSM0XFEiwkdnF0qwtHGzmYARYhL1Tpp/SuSq7EsJvu0UrYl+hJpV+4VbU08M7YsEEwHAQkolKxgJZf6x/A8cliAIoMnrAoZ0a15/GBgadmuqUy1RkR0Lfr5ta4xEriqeYt+uiaZ84hCSVq+k6MX1P0b23ytqdOJXrvjsasDfPuTojvg+pyylZRj2Fz+MlVM3SnEzfvpKGuY7wbVxtg7kcKdL3wXqJZoUoIYGgr1buxO6iLa2784xfUdSK5iu1YA+B2tpxSxSz8= berg@Izanami"
+        ];
       };
     };
     wireguard = {
