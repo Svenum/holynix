3d2300a755887ba7a7b00ac7ac4f50f8e04e04cf
Svenum <s.ziegler@holypenguin.net>
2025-01-28 11:03:34 +0100

    patch kvmfr

--- overlays/kvmfr/default.nix
+++ overlays/kvmfr/default.nix
@@ -1,19 +1,30 @@
 { ... }:
 
 final: prev: {
-  linuxPackages = prev.linuxPackages_6_13.extend (lfinal: lprev: {
-    kvmfr = prev.linuxPackages_6_13.kvmfr.overrideAttrs (old: {
-      patches = ( old.patches or [] ) ++ [
+  linuxPackages = prev.linuxPackages.extend (lfinal: lprev: {
+    kvmfr = prev.linuxPackages.kvmfr.overrideAttrs (old: {
+      patches = [
+        #(prev.fetchpatch {
+        #  url = "https://github.com/gnif/LookingGlass/commit/cf985536a6f80fe985e3eac3fd0396da12f41ad3.patch";
+        #  hash = "sha256-4vlUkIg8Na70WzyaZATELFEVMbATh6RTBziwontzybE=";
+        #  stripLen = 1;
+        #})
         (prev.fetchpatch {
           url = "https://github.com/gnif/LookingGlass/commit/7740692e3000c2019e21b9861585960174dd5ddc.patch";
-          hash = "05h2z3xh3qmhaifhanl59ns3xk5di1ark9ibbkmhnj6416hs7lwa";
-        })
-
-        (prev.fetchpatch {
-          url = "https://github.com/gnif/LookingGlass/commit/cf985536a6f80fe985e3eac3fd0396da12f41ad3.patch";
-          hash = "0cbil1ak9qpg3rs65a6azxz6dffhgq4yqrmnldfgada0rsaac2j3";
+          hash = "sha256-QRg92WttsROFT8ZxUp9JeGPEZOhdZfznFBkAL8EQr+k=";
+          stripLen = 1;
         })
       ];
     });
+  });
+
+  looking-glass-client = prev.looking-glass-client.overrideAttrs (old: {
+    version = "unstable";
+    src = prev.fetchFromGitHub {
+      owner = "gnif";
+      repo = "LookingGlass";
+      hash = "sha256-efAO7KLdm7G4myUv6cS1gUSI85LtTwmIm+HGZ52arj8=";
+      rev = "master";
+    };
   });
 }
