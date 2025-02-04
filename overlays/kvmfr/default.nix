ba58c221b704c8eccbdb775419241205b99912d4
Svenum <s.ziegler@holypenguin.net>
2025-02-04 12:21:10 +0100

    update flake + add flake check action

--- overlays/kvmfr/default.nix
+++ overlays/kvmfr/default.nix
@@ -5,15 +5,10 @@ final: prev: {
     kvmfr = prev.linuxPackages_latest.kvmfr.overrideAttrs (old: {
       patches = [
         (prev.fetchpatch {
-          url = "https://github.com/gnif/LookingGlass/commit/7740692e3000c2019e21b9861585960174dd5ddc.patch";
-          hash = "sha256-QRg92WttsROFT8ZxUp9JeGPEZOhdZfznFBkAL8EQr+k=";
+          url = "https://github.com/gnif/LookingGlass/commit/4251a5c5fe7723c5dc068839debd76a5148953b2.patch";
+          hash = "sha256-xRg92WttsROFT8ZxUp9JeGPEZOhdZfznFBkAL8EQr+k=";
           stripLen = 1;
         })
-        #(prev.fetchpatch {
-        #  url = "https://github.com/gnif/LookingGlass/commit/cf985536a6f80fe985e3eac3fd0396da12f41ad3.patch";
-        #  hash = "sha256-4vlUkIg8Na70WzyaZATELFEVMbATh6RTBziwontzybE=";
-        #  stripLen = 1;
-        #})
       ];
     });
   });
