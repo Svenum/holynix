f2a325c9fa887d7d34ce96810c5b31a0be8c78ed
Svenum <s.ziegler@holypenguin.net>
2025-02-13 09:03:13 +0100

    update tail-tray

--- packages/tail-tray/default.nix
+++ packages/tail-tray/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   name = "tail-tray";
-  version = "0.2.10";
+  version = "0.2.11";
 
   src = fetchFromGitHub {
     owner = "SneWs";
     repo = "tail-tray";
     tag = "v${version}";
-    sha256 = "sha256-60ddnIyW93TKHlZ4PgW1ggoyN4moHJEiQ9FnnL6cM7Q=";
+    sha256 = "sha256-khJX/zsbDgT+eJVhIR72i8kYGJVoUEngfjZp/TASa/4=";
   };
 
   nativeBuildInputs = with kdePackages; [
@@ -30,19 +30,6 @@ stdenv.mkDerivation rec {
     pkg-config
     qtbase
     davfs2
-  ];
-
-  patches = [
-    # Fixes https://github.com/SneWs/tail-tray/issues/33
-    (fetchpatch {
-      url = "https://github.com/SneWs/tail-tray/commit/d7655e2187f6a445bd046d0b6fc766387a4d55b7.diff";
-      hash = "sha256-hyf3f8A8hxx2WFsh0eYb4meytg0TpOh9nZAUAtL0jsY=";
-    })
-    # Fiexes https://github.com/SneWs/tail-tray/issues/37
-    (fetchpatch {
-      url = "https://github.com/SneWs/tail-tray/commit/83719c629ea98a035ff937a819d6c493f5a7c032.patch";
-      hash = "sha256-oqePwgo+ejhnwh3RZ8+gnwOV2M7w/q04cc16nuQVI3E=";
-    })
   ];
 
   postFixup = ''
