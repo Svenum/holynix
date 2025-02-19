def872f7a00612ea0dcefb5c4b731ca89ee1c39b
Svenum <s.ziegler@holypenguin.net>
2025-02-19 15:42:55 +0100

    tetris: init at 0.1.6

--- packages/tetris/default.nix
+++ packages/tetris/default.nix
@@ -1,47 +1,57 @@
 { haskellPackages }:
 
 haskellPackages.callPackage (
-{
-  mkDerivation,
-  fetchgit,
-  lib,
+  {
+    mkDerivation,
+    fetchFromGitHub,
+    lib,
 
-  base,
-  vty,
-  lens,
-  brick,
-  linear,
-  containers,
-  random,
-  transformers,
-  directory,
-  filepath,
-  optparse-applicative,
-  vty-crossplatform,
-  mtl,
-  extra
-}:
-mkDerivation {
-  pname = "tetris";
-  version = "0.1.6";
-  src = fetchgit {
-    url = "https://github.com/Samtay/tetris.git";
-    sha256 = "sha256-jkzSoAQWeSEG91GWe3WFpLZwK1JK3WWDQkHxeyG4OJ4=";
-    rev = "0.1.6";
-    fetchSubmodules = true;
-  };
-  isLibrary = true;
-  isExecutable = true;
-  libraryHaskellDepends = [
-    base brick containers extra lens linear mtl random transformers vty
-    vty-crossplatform
-  ];
-  executableHaskellDepends = [
-    base directory filepath optparse-applicative
-  ];
-  homepage = "https://github.com/samtay/tetris#readme";
-  license = lib.licenses.bsd3;
-  mainProgram = "tetris";
-  maintainers = [ lib.maintainers.Svenum ];
-}) {}
-
+    base,
+    vty,
+    lens,
+    brick,
+    linear,
+    containers,
+    random,
+    transformers,
+    directory,
+    filepath,
+    optparse-applicative,
+    vty-crossplatform,
+    mtl,
+    extra,
+  }:
+  mkDerivation rec {
+    pname = "tetris";
+    version = "0.1.6";
+    src = fetchFromGitHub {
+      repo = "tetris";
+      owner = "Samtay";
+      sha256 = "sha256-xA2/n5zY01BLKlUI8BVvfuUvsqh2U23XOooTQwXkDpQ=";
+      tag = "v${version}";
+    };
+    libraryHaskellDepends = [
+      base
+      brick
+      containers
+      extra
+      lens
+      linear
+      mtl
+      random
+      transformers
+      vty
+      vty-crossplatform
+    ];
+    executableHaskellDepends = [
+      base
+      directory
+      filepath
+      optparse-applicative
+    ];
+    homepage = "https://github.com/samtay/tetris#readme";
+    license = lib.licenses.bsd3;
+    mainProgram = "tetris";
+    maintainers = [ lib.maintainers.Svenum ];
+  }
+) { }
