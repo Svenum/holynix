711905f53db9d1a1a3955b4879bb1e2d9d8411a8
Svenum <s.ziegler@holypenguin.net>
2025-03-10 11:40:23 +0100

    home/tmux: add extraConfig

--- modules/home/tools/tmux/default.nix
+++ modules/home/tools/tmux/default.nix
@@ -83,8 +83,7 @@ in
       set -g @catppuccin_window_current_middle_separator "#[bg=#{@catppuccin_window_current_text_color},fg=#{@catppuccin_window_current_number_color}]"
       
       set -g @catppuccin_status_left_separator "#[none] #[fg=#{@_ctp_status_bg}]"
-      set -g @catppuccin_status_middle_separator "#[bg=#{@catppuccin_window_text_color},fg=#{@catppuccin_window_number_color}]"
-      set -g @catppuccin_status_current_middle_separator "#[bg=#{@catppuccin_window_current_text_color},fg=#{@catppuccin_window_current_number_color}]"
+      set -g @catppuccin_status_right_separator ""
     '';
   };
 }
