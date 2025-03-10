1e052aeb6a1752852484e287e16118f71d177960
Svenum <s.ziegler@holypenguin.net>
2025-03-10 11:30:09 +0100

    home/tmux: add extraConfig

--- modules/home/tools/tmux/default.nix
+++ modules/home/tools/tmux/default.nix
@@ -42,9 +42,11 @@ in
     catppuccin.tmux.extraConfig = ''
       set -g @catppuccin_window_status_style "custom"
 
+      set -g window-status-separator ""
+      set -g status-left-length 0
+      set -g status-left "#[fg=#{@thm_fg} bold]TMUX (#S) "
+      set -ga status-left "#{?client_prefix,#[fg=#{@thm_red} bold]PREFIX ,#{?#{==:#{pane_mode},copy-mode},#[fg=#{@thm_yellow} bold]COPY ,#[fg=#{@thm_green} bold]NORMAL }}"
       set -g status-right-length 100
-      set -g status-left-length 100
-      set -g status-left ""
       set -g status-right "#{E:@catppuccin_status_application}"
       set -ag status-right "#{E:@catppuccin_status_session}"
       set -ag status-right "#{E:@catppuccin_status_user}"
@@ -53,19 +55,32 @@ in
 
       set -g @catppuccin_date_time_text " %d.%m.%Y %H:%M"
 
-      set -g @catppuccin_window_left_separator " "
-      set -g @catppuccin_window_middle_separator " "
-      set -g @catppuccin_window_right_separator " "
+      #set -g @catppuccin_window_left_separator " "
+      #set -g @catppuccin_window_middle_separator " "
+      #set -g @catppuccin_window_right_separator " "
 
-      set -g @catppuccin_status_left_separator " "
-      set -g @catppuccin_status_right_separator "█"
+      #set -g @catppuccin_status_left_separator " "
+      #set -g @catppuccin_status_right_separator "█"
 
-      set -g @catppuccin_status_fill "all"
-      set -g @catppuccin_status_connect_separator "no"
-      set -g @catppuccin_window_current_text "#{pane_current_path}"
-      set -g @catppuccin_window_number_position "right"
-      set -g @catppuccin_window_default_fill "number"
-      set -g @catppuccin_window_current_fill "all"
+      #set -g @catppuccin_status_fill "all"
+      #set -g @catppuccin_status_connect_separator "no"
+      #set -g @catppuccin_window_current_text "#{pane_current_path}"
+      #set -g @catppuccin_window_number_position "right"
+      #set -g @catppuccin_window_default_fill "number"
+      #set -g @catppuccin_window_current_fill "all"
+
+      set -g @catppuccin_window_number_position "left"
+      set -g @catppuccin_window_current_number_color "#{@thm_green}"
+      set -g @catppuccin_window_text ""
+      set -g @catppuccin_window_number "#[bold]Tab ###I "
+      set -g @catppuccin_window_current_text ""
+      set -g @catppuccin_window_current_number "#[bold]Tab ###I "
+      set -g @catppuccin_window_status_style "custom"
+      set -g @catppuccin_window_right_separator "#[fg=#{@_ctp_status_bg},reverse]#[none]"
+
+      set -g @catppuccin_window_left_separator "#[fg=#{@_ctp_status_bg}] #[none]"
+      set -g @catppuccin_window_middle_separator "#[bg=#{@catppuccin_window_text_color},fg=#{@catppuccin_window_number_color}]"
+      set -g @catppuccin_window_current_middle_separator "#[bg=#{@catppuccin_window_current_text_color},fg=#{@catppuccin_window_current_number_color}]"
     '';
   };
 }
