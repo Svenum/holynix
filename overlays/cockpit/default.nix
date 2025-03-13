3b082e409bda30cd29a4de2df23bcb2d3185b688
Svenum <s.ziegler@holypenguin.net>
2025-03-13 18:04:32 +0100

    overlays + modules: remove cockpit

--- overlays/cockpit/default.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-
-_final: prev: {
-  cockpit = prev.cockpit.overrideAttrs (old:{
-    # Remove packagekit from cockpit so that it does not look for updates
-    fixupPhase = builtins.replaceStrings [ "runHook postFixup" ] [ "rm -rf $out/share/cockpit/packagekit\nrunHook postFixup" ] old.fixupPhase;
-  });
-}
