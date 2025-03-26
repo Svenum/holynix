77f3d9c04899440937b8f303030c41fb81ad4bca
Svenum <s.ziegler@holypenguin.net>
2025-03-26 09:03:54 +0100

    home/tmux: update config

--- modules/home/tools/tmux/default.nix
+++ modules/home/tools/tmux/default.nix
@@ -56,7 +56,7 @@ in
       set -g @catppuccin_window_text "#{window_name}"
 
       ## Window current configuration
-      set -g @catppuccin_window_current_text "#[fg=#{@catppuccin_window_current_number_color},reverse]#{window_name}#[none]"
+      set -g @catppuccin_window_current_text "#[bg=#{@catppuccin_window_current_number_color},reverse]#{window_name}#[none]"
       set -g @catppuccin_window_current_number "#[reverse] #I#[none]"
       set -g @catppuccin_window_current_middle_separator "#[fg=#{@catppuccin_window_current_number_color},bg=#{@thm_bg},reverse] 󰿟 #[none]"
       set -g @catppuccin_window_current_right_separator "#[fg=#{@catppuccin_window_current_number_color},bg=#{@thm_bg}]█#[none]"
