21404516c16364682ceef8624ed49716b8668640
Svenum <s.ziegler@holypenguin.net>
2025-04-24 17:39:43 +0200

    add shin-br0

--- systems/aarch64-linux/srv-raspi5/default.nix
+++ systems/aarch64-linux/srv-raspi5/default.nix
@@ -50,5 +50,11 @@ in
     defaultGateway = "172.16.0.1";
     nameservers = [ "172.16.0.3" "172.16.0.4" ];
     useDHCP = false;
+    localCommands = ''
+      ip l add shim-br0 link br0 type ipvlan mode l3n
+      ip l set shim-br0 up
+      ip a add 172.16.0.13/24 dev shim-br0
+      ip r append 172.16.0.0/24 dev br0 proto kernel scope link src 172.16.0.13 metric 1
+    '';
   };
 }
