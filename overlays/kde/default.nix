b8c027bd829d097d4753056724b4956350447b76
Svenum <s.ziegler@holypenguin.net>
2025-09-29 11:51:21 +0200

    overlays/plasma-workspace: remove

--- overlays/kde/default.nix
+++ overlays/kde/default.nix
@@ -5,13 +5,5 @@ _final: prev: {
     kdeconnect-kde = kprev.kdeconnect-kde.overrideAttrs (old: {
       cmakeFlags = (old.cmakeFlags or [ ]) ++ [ "-DBLUETOOTH_ENABLED=ON" ];
     });
-    plasma-workspace = kprev.plasma-workspace.overrideAttrs (old: {
-      patches = (old.patches or [ ]) ++ [
-        (prev.fetchpatch {
-          url = "https://invent.kde.org/plasma/plasma-workspace/-/commit/30273fb2afcc6e304951c8895bb17d38255fed39.patch";
-          hash = "sha256-1p1CjxRioCDm5ugoI8l6kDlOse5FbDJ71tTAY9LPvRc=";
-        })
-      ];
-    });
   });
 }
