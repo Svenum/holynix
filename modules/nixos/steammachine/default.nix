bbddf0a73061fa9cfc2755c610d388319e100586
Svenum <s.ziegler@holypenguin.net>
2025-10-09 12:50:37 +0200

    add login script

--- modules/nixos/steammachine/default.nix
+++ modules/nixos/steammachine/default.nix
@@ -1,9 +1,13 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 with lib.types;
 let
   cfg = config.holynix.steammachine;
+  gs = pkgs.writeScriptBin "gs.sh" ''
+    #!/usr/bin/env bash
+    exec gamescope --adaptive-sync --hdr-enabled --rt --steam -- flatpak run com.valvesoftware.Steam -pipewire-dmabuf -tenfoot -bigpicture
+  '';
 in
 {
   options.holynix.steammachine = {
@@ -26,12 +30,21 @@ in
       isKvmUser = false;
     };
 
+    environment = {
+      loginShellInit = ''
+        [[ "$(tty)" = "/dev/tty9" ]] && ${gs}/bin/gs.sh
+      '';
+      systemPackages = with pkgs; [
+        gamescope-wsi
+      ];
+    };
+
     systemd.services."steammachine-getty" = {
       description = "Getty for tty9 for the steammachine";
       wantedBy = [ "getty.target" ];
       after = [ "systemd-user-sessions.service" ];
       serviceConfig = {
-        ExecStart = [ "/sbin/agetty --autologin steam --noclear tty9 linux" ];
+        ExecStart = [ "${pkgs.util-linux}/bin/agetty --autologin steam --noclear tty9 linux" ];
         Type = "idle";
         Restart = "always";
       };
