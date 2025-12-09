666005358992af6960e94fb47f631a7c3021a28a
Svenum <s.ziegler@holypenguin.net>
2025-12-09 13:10:48 +0100

    allow unfree

--- modules/nixos/users/default.nix
+++ modules/nixos/users/default.nix
@@ -32,6 +32,7 @@ let
 
   mkUserConfig = name: user: {
     # Home-Manager Config
+    nixpkgs.config.allowUnfree = true;
     home = {
       username = name;
       homeDirectory = "/home/${name}";
