2f24aab06630ca26567d144afc5df338a0c486db
Svenum <s.ziegler@holypenguin.net>
2026-01-20 14:18:28 +0100

    add tunnel

--- systems/x86_64-linux/Kaeru/container/cloudflare.nix
+++ systems/x86_64-linux/Kaeru/container/cloudflare.nix
@@ -7,29 +7,56 @@
     key = "";
   };
 
-  virtualisation.quadlet = {
-    containers = {
-      "cloudflare_ddns" = {
-        autoStart = true;
-        containerConfig = {
-          image = "docker.io/favonia/cloudflare-ddns:latest";
-          environments = {
-            PROXIED = "false";
-            IP6_PROVIDER = "none";
+  virtualisation.quadlet =
+    let
+      inherit (config.virtualisation.quadlet) networks;
+    in
+    {
+      containers = {
+        "cloudflare_ddns" = {
+          autoStart = true;
+          containerConfig = {
+            image = "docker.io/favonia/cloudflare-ddns:latest";
+            environments = {
+              PROXIED = "false";
+              IP6_PROVIDER = "none";
+            };
+            environmentFiles = [ config.sops.secrets."cloudflare.env".path ];
+            networks = [
+              "host"
+            ];
+            addCapabilities = [
+              "SETUID"
+              "SETGID"
+            ];
+            dropCapabilities = [ "all" ];
+            noNewPrivileges = true;
+            readOnly = true;
           };
-          environmentFiles = [ config.sops.secrets."cloudflare.env".path ];
-          networks = [
-            "host"
-          ];
-          addCapabilities = [
-            "SETUID"
-            "SETGID"
-          ];
-          dropCapabilities = [ "all" ];
-          noNewPrivileges = true;
-          readOnly = true;
+        };
+        "cloudflare_tunnel" = {
+          autoStart = true;
+          containerConfig = {
+            image = "docker.io/cloudflare/cloudflared:latest";
+            environmentFiles = [ config.sops.secrets."cloudflare.env".path ];
+            networks = [
+              networks.proxy.ref
+              networks.cloudflare_default.ref
+            ];
+            exec = "tunnel run";
+            dns = [
+              "172.16.0.3"
+              "172.16.0.4"
+            ];
+          };
+        };
+      };
+      networks.cloudflare_default = {
+        autoStart = true;
+        networkConfig = {
+          driver = "bridge";
+          interfaceName = "pod_cloudflare";
         };
       };
     };
-  };
 }
