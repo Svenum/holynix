32897886d90a8cc86b3125a697ed25cfdea934ef
Svenum <s.ziegler@holypenguin.net>
2026-02-04 17:37:53 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 nixos-install \
-  --option substituters "https://iglu.holypenguin.net/default https://nix-community.cachix.org" \
-  --option trusted-public-keys "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= default:xZJwKM6k5SCrviOA50/5RKldgPHRPkOrv/uziJVAm2U=" "$@"
+  --option extra-substituters "https://iglu.holypenguin.net/default https://nix-community.cachix.org" \
+  --option extra-trusted-public-keys "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= default:xZJwKM6k5SCrviOA50/5RKldgPHRPkOrv/uziJVAm2U=" "$@"
