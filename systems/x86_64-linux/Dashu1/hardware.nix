0d12c9a9f7d5bdb981ba060ca6657061647daf33
Svenum <s.ziegler@holypenguin.net>
2026-02-04 18:17:29 +0100

    update Dashus

--- systems/x86_64-linux/Dashu1/hardware.nix
+++ systems/x86_64-linux/Dashu1/hardware.nix
@@ -16,19 +16,37 @@
       ];
       kernelModules = [ ];
     };
-    kernelModules = [ ];
+    kernelModules = [ "kvm-amd" ];
     extraModulePackages = [ ];
   };
 
   # Filesystems
   fileSystems = {
     "/" = {
-      device = "/dev/vda2";
-      fsType = "ext4";
+      device = "/dev/disk/by-uuid/a39bfdb5-4c1f-491e-b33a-4886651391de";
+      fsType = "btrfs";
+      options = [ "subvol=@root" ];
     };
+
+    "/nix" = {
+      device = "/dev/disk/by-uuid/a39bfdb5-4c1f-491e-b33a-4886651391de";
+      fsType = "btrfs";
+      options = [ "subvol=@nix" ];
+    };
+
+    "/var/log" = {
+      device = "/dev/disk/by-uuid/a39bfdb5-4c1f-491e-b33a-4886651391de";
+      fsType = "btrfs";
+      options = [ "subvol=@var-log" ];
+    };
+
     "/boot" = {
-      device = "/dev/vda1";
+      device = "/dev/disk/by-uuid/A124-2919";
       fsType = "vfat";
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
     };
   };
 
