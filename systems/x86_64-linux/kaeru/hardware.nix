f7624d2634846a20ba9d590a3f194aacfdafb850
Svenum <s.ziegler@holypenguin.net>
2026-03-27 10:17:36 +0100

    add hardware modulels

--- systems/x86_64-linux/kaeru/hardware.nix
+++ systems/x86_64-linux/kaeru/hardware.nix
@@ -1,10 +1,29 @@
-_:
+{ lib, ... }:
 
 {
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "uhci_hcd"
+        "ehci_pci"
+        "ahci"
+        "virtio_pci"
+        "sr_mod"
+        "virtio_blk"
+      ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+  };
+
   fileSystems = {
     "/".device = "/dev/vda2";
   };
   fileSystems = {
     "/boot".device = "/dev/vda1";
   };
+
+  swapDevices = [ ];
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
