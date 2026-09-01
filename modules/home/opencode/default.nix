ed84f3703b9490216424e27c50c935ba2f9ffbd4
Svenum <s.ziegler@holypenguin.net>
2026-09-01 11:44:28 +0100

    home/opencode: set default model

--- modules/home/opencode/default.nix
+++ modules/home/opencode/default.nix
@@ -18,12 +18,18 @@ in
     programs.opencode = {
       enable = true;
       settings = {
+        model = "ollama/LisyNeko/qwen3.8-9b-coder:latest";
         provider = {
           ollama = {
             npm = "@ai-sdk/openai-compatible";
             name = "Ollama";
             options = {
               baseURL = "http://localhost:11434/v1";
+            };
+            models = {
+              "LisyNeko/qwen3.8-9b-coder:latest" = {
+                name = "Qwen3.8 9B Coder";
+              };
             };
           };
         };
